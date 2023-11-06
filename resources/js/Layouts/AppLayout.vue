639b3113ef91ff85d6ae30ec7341fe7eab39fcea
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2023-11-06 19:32:49 -0300

    add bookmark link

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -51,6 +51,9 @@ const logout = () => {
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('bookmark')" :active="route().current('bookmark')">
+                                   Bookmark
+                                </NavLink>
                             </div>
                         </div>
 
