5039de0c7a1a8b65d1aeaa5d3fae974b15bede85
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2023-11-08 22:12:02 -0300

    add use trait tag to model

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
 
 class Bookmark extends Model
 {
-    use HasFactory;
+    use HasFactory,HasTags;
+
     protected $fillable = ['title', 'uri', 'description'];
 }
