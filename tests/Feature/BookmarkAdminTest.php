2f7073a627aaa0b038c8a78fa6bbfe8f74f3e0cb
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2023-11-11 19:07:06 -0300

    add test to creating tags

--- tests/Feature/BookmarkAdminTest.php
+++ tests/Feature/BookmarkAdminTest.php
@@ -31,4 +31,32 @@ class BookmarkAdminTest extends TestCase
         );
         //$response->assertSee($bookmark->title);
     }
+
+    public function test_add_new_bookmark_successfully(): void
+    {
+        $tags = ['tag1', 'tag2', 'tag3'];
+        $this->actingAs($user = User::factory()->create());
+
+        $response = $this->post(route('bookmark.store'), [
+            'title' => 'Example Title',
+            'uri' => 'http://example.com',
+            'description' => 'Example description',
+            'tags' => $tags
+        ]);
+
+        $response->assertStatus(302);
+        $this->assertDatabaseHas('bookmarks', [
+            'title' => 'Example Title',
+            'uri' => 'http://example.com',
+            'description' => 'Example description'
+        ]);
+
+        $newRecord = Bookmark::latest()->first();
+        foreach ($tags as $tag) {
+            $this->assertDatabaseHas('tags', ['name->en' => $tag]);
+            $this->assertTrue($newRecord->tags->contains('name', $tag));
+        }
+
+    }
+
 }
