b23483dc9832767ce8184019132e50f151d24d0c
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2023-12-01 16:46:56 -0300

    add scope public

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -4,10 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Spatie\Tags\HasTags;
 
 class Bookmark extends Model
 {
     use HasFactory, HasTags;
-    protected $fillable = ['title', 'uri', 'description'];
+    protected $fillable = ['title', 'uri', 'description', 'private'];
+
+    protected static function booted(){
+
+      static::creating(function($model){
+        if ( $model->private == null ) {
+          $model->private = 0;
+        }
+      });
+
+      static::updating(function($model){
+        if ( $model->private == null ) {
+          $model->private = 0;
+        }
+      });
+
+    }
+
+    public function scopePublic(Builder $query): void {
+      $query->where('private', 0);
+    }
 }
