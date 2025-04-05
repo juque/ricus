4b5d600004f9efd86d0daaa0381c4096323f634d
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2025-04-04 23:04:38 -0300

    improve formatting

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,24 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
-use Spatie\Tags\Tag;
-use Inertia\Inertia;
 use App\Models\Bookmark;
 
 class TagController extends Controller
 {
-    public function __invoke(Request $request, $tag)
-		{
-      $bookmarks = auth()->check()
-        ? Bookmark::withAnyTags([$tag])->get()
-        : Bookmark::public()->withAnyTags([$tag])->get();
+    public function __invoke(string $tag)
+    {
+        $bookmarks = auth()->check()
+          ? Bookmark::withAnyTags([$tag])->get()
+          : Bookmark::public()->withAnyTags([$tag])->get();
 
-				return inertia()->render('Tag', [
-          'bookmarks' => $bookmarks,
-          'tagged' => $tag
-				]
-			);
+        return inertia()
+          ->render(
+              'Tag',
+              ['bookmarks' => $bookmarks, 'tagged' => $tag]
+          );
     }
 }
