a11e635b594dfcaed0aa924ec5798b73907b33cc
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2025-04-06 22:05:18 -0400

    showing tags

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,23 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Bookmark;
 use Spatie\Tags\Tag;
 
 class WelcomeController extends Controller
 {
-  public function __invoke(Request $request)
-  {
-    $tagList = Tag::all();
+    public function __invoke()
+    {
+        $tagList = Tag::all();
 
-    $bookmarks = auth()->check() 
-      ? Bookmark::with('tags')->latest()->paginate(3)
-      : Bookmark::public()->with('tags')->latest()->paginate(3);
+        $bookmarks = auth()->check()
+          ? Bookmark::with('tags')->latest()->paginate(3)
+          : Bookmark::public()->with('tags')->latest()->paginate(3);
 
-    return inertia()->render('Welcome', [
-      'bookmarks' => $bookmarks,
-      'tagList'   => $tagList
-    ]);
-  }
+        return inertia()->render('Welcome', [
+          'bookmarks' => $bookmarks,
+          'tagList'   => $tagList
+        ]);
+    }
 }
